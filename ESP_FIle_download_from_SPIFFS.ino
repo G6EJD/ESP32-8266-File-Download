8e7eb8995a167dd2ddff2bd443fb50591a0a653d
G6EJD <d.l.bird@sky.com>
2019-04-22 09:51:52 +0100

    SPIFFS Version

---
+++ ESP_FIle_download_from_SPIFFS.ino
@@ -0,0 +1,160 @@
+/*  Version 1
+
+    ESP32/ESP8266 example of downloading a file from the device's SPIFFS Filing System
+
+  This software, the ideas and concepts is Copyright (c) David Bird 2018. All rights to this software are reserved.
+
+  Any redistribution or reproduction of any part or all of the contents in any form is prohibited other than the following:
+  1. You may print or download to a local hard disk extracts for your personal and non-commercial use only.
+  2. You may copy the content to individual third parties for their personal use, but only if you acknowledge the author David Bird as the source of the material.
+  3. You may not, except with my express written permission, distribute or commercially exploit the content.
+  4. You may not transmit it or store it in any other website or other form of electronic retrieval system for commercial purposes.
+
+  The above copyright ('as annotated') notice and this permission notice shall be included in all copies or substantial portions of the Software and where the
+  software use is visible to an end-user.
+
+  THE SOFTWARE IS PROVIDED "AS IS" FOR PRIVATE USE ONLY, IT IS NOT FOR COMMERCIAL USE IN WHOLE OR PART OR CONCEPT. FOR PERSONAL USE IT IS SUPPLIED WITHOUT WARRANTY
+  OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  IN NO EVENT SHALL THE AUTHOR OR COPYRIGHT HOLDER BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  See more at http://www.dsbird.org.uk
+
+*/
+#ifdef ESP8266
+#include <ESP8266WiFi.h>       // Built-in
+#include <ESP8266WiFiMulti.h>  // Built-in
+#include <ESP8266WebServer.h>  // Built-in
+#include <ESP8266mDNS.h>
+#include "FS.h"
+#else
+#include <WiFi.h>              // Built-in
+#include <WiFiMulti.h>         // Built-in
+#include <ESP32WebServer.h>    // https://github.com/Pedroalbuquerque/ESP32WebServer download and place in your Libraries folder
+#include <ESPmDNS.h>
+#include "FS.h"
+#endif
+
+#include "Network.h"
+#include "Sys_Variables.h"
+#include "CSS.h"
+#include <SPI.h>
+
+#ifdef ESP8266
+ESP8266WiFiMulti wifiMulti;
+ESP8266WebServer server(80);
+#else
+WiFiMulti wifiMulti;
+ESP32WebServer server(80);
+#endif
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void setup(void) {
+  Serial.begin(115200);
+  if (!WiFi.config(local_IP, gateway, subnet, dns)) { //WiFi.config(ip, gateway, subnet, dns1, dns2);
+    Serial.println("WiFi STATION Failed to configure Correctly");
+  }
+  wifiMulti.addAP(ssid_1, password_1);  // add Wi-Fi networks you want to connect to, it connects strongest to weakest
+  wifiMulti.addAP(ssid_2, password_2);  // Adjust the values in the Network tab
+  wifiMulti.addAP(ssid_3, password_3);
+  wifiMulti.addAP(ssid_4, password_4);  // You don't need 4 entries, this is for example!
+
+  Serial.println("Connecting ...");
+  while (wifiMulti.run() != WL_CONNECTED) { // Wait for the Wi-Fi to connect: scan for Wi-Fi networks, and connect to the strongest of the networks above
+    delay(250); Serial.print('.');
+  }
+  Serial.println("\nConnected to " + WiFi.SSID() + " Use IP address: " + WiFi.localIP().toString()); // Report which SSID and IP is in use
+  // The logical name http://fileserver.local will also access the device if you have 'Bonjour' running or your system supports multicast dns
+  if (!MDNS.begin(servername)) {          // Set your preferred server name, if you use "myserver" the address would be http://myserver.local/
+    Serial.println(F("Error setting up MDNS responder!"));
+    ESP.restart();
+  }
+#ifdef ESP32
+  // Note: SD_Card readers on the ESP32 will NOT work unless there is a pull-up on MISO, either do this or wire one on (1K to 4K7)
+  Serial.println(MISO);
+  pinMode(19, INPUT_PULLUP);
+#endif
+  Serial.print(F("Initializing SD card..."));
+  SPIFFS.begin();                           // Start the SPI Flash Files System
+  // Note: Using the ESP32 and SD_Card readers requires a 1K to 4K7 pull-up to 3v3 on the MISO line, otherwise they do-not function.
+  //----------------------------------------------------------------------
+  ///////////////////////////// Server Commands
+  server.on("/",         HomePage);
+  server.on("/download", File_Download);
+  ///////////////////////////// End of Request commands
+  server.begin();
+  Serial.println("HTTP server started");
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void loop(void) {
+  server.handleClient(); // Listen for client connections
+}
+
+// All supporting functions from here...
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void HomePage() {
+  SendHTML_Header();
+  webpage += F("<a href='/download'><button>Download</button></a>");
+  append_page_footer();
+  SendHTML_Content();
+  SendHTML_Stop(); // Stop is needed because no content length was sent
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void File_Download() { // This gets called twice, the first pass selects the input, the second pass then processes the command line arguments
+  if (server.args() > 0 ) { // Arguments were received
+    if (server.hasArg("download")) SD_file_download(server.arg(0));
+  }
+  else SelectInput("File Download", "Enter filename to download", "download", "download");
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void SD_file_download(String filename) {
+  File download = SPIFFS.open("/" + filename, "w");
+  if (download) {
+    server.sendHeader("Content-Type", "text/text");
+    server.sendHeader("Content-Disposition", "attachment; filename=" + filename);
+    server.sendHeader("Connection", "close");
+    server.streamFile(download, "application/octet-stream");
+    download.close();
+  } else ReportFileNotPresent("download");
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void SendHTML_Header() {
+  server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  server.sendHeader("Pragma", "no-cache");
+  server.sendHeader("Expires", "-1");
+  server.setContentLength(CONTENT_LENGTH_UNKNOWN);
+  server.send(200, "text/html", ""); // Empty content inhibits Content-length header so we have to close the socket ourselves.
+  append_page_header();
+  server.sendContent(webpage);
+  webpage = "";
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void SendHTML_Content() {
+  server.sendContent(webpage);
+  webpage = "";
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void SendHTML_Stop() {
+  server.sendContent("");
+  server.client().stop(); // Stop is needed because no content length was sent
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void SelectInput(String heading1, String heading2, String command, String arg_calling_name) {
+  SendHTML_Header();
+  webpage += F("<h3 class='rcorners_m'>"); webpage += heading1 + "</h3><br>";
+  webpage += F("<h3>"); webpage += heading2 + "</h3>";
+  webpage += F("<FORM action='/"); webpage += command + "' method='post'>"; // Must match the calling argument e.g. '/chart' calls '/chart' after selection but with arguments!
+  webpage += F("<input type='text' name='"); webpage += arg_calling_name; webpage += F("' value=''><br>");
+  webpage += F("<type='submit' name='"); webpage += arg_calling_name; webpage += F("' value=''><br><br>");
+  append_page_footer();
+  SendHTML_Content();
+  SendHTML_Stop();
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void ReportFileNotPresent(String target) {
+  SendHTML_Header();
+  webpage += F("<h3>File does not exist</h3>");
+  webpage += F("<a href='/"); webpage += target + "'>[Back]</a><br><br>";
+  append_page_footer();
+  SendHTML_Content();
+  SendHTML_Stop();
+}
